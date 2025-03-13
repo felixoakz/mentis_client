ccc8c367ac5ad3a0cb8bfe05c69051874938ea63
felix <felix@oakz.org>
2025-03-13 20:41:35 -0300

    adding /api/ on dev

--- src/api/base.js
+++ src/api/base.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:7000",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:7000/api/",
   withCredentials: true,
   headers: {
     'Accept': 'application/json',
