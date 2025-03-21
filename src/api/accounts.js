c9e739047cbb4fb9caeb575912474dfe46e56349
felix <felix@oakz.org>
2025-03-21 16:59:37 -0300

    removes mentis from path

--- src/api/accounts.js
+++ src/api/accounts.js
@@ -2,18 +2,18 @@ import apiClient from "./base";
 
 
 export const listAccounts = async () => {
-  return await apiClient.get('/mentis/accounts')
+  return await apiClient.get('/accounts')
 }
 
 export const createAccount = async (accountName) => {
   console.log(accountName)
-  return await apiClient.post('/mentis/accounts', accountName)
+  return await apiClient.post('/accounts', accountName)
 }
 
 export const editAccount = async (accountId, accountName) => {
-  return await apiClient.put(`/mentis/accounts/${accountId}`, accountName)
+  return await apiClient.put(`/accounts/${accountId}`, accountName)
 }
 
 export const deleteAccount = async (accountId) => {
-  return await apiClient.delete(`/mentis/accounts/${accountId}`)
+  return await apiClient.delete(`/accounts/${accountId}`)
 }
--- src/api/transactions.js
+++ src/api/transactions.js
@@ -2,17 +2,17 @@ import apiClient from "./base";
 
 
 export const listTransactions = async (accountId) => {
-  return await apiClient.get(`/mentis/transactions/${accountId}`)
+  return await apiClient.get(`/transactions/${accountId}`)
 }
 
 export const createTransaction = async (transactionData) => {
-  return await apiClient.post(`/mentis/transactions`, transactionData)
+  return await apiClient.post(`/transactions`, transactionData)
 }
 
 export const editTransaction = async (transactionId, transactionData) => {
-  return await apiClient.put(`/mentis/transactions/${transactionId}`, transactionData)
+  return await apiClient.put(`/transactions/${transactionId}`, transactionData)
 }
 
 export const deleteTransaction = async (transactionId) => {
-  return await apiClient.delete(`/mentis/transactions/${transactionId}`)
+  return await apiClient.delete(`/transactions/${transactionId}`)
 }
